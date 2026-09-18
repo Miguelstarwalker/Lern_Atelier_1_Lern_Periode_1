81ee502e9cd94d900717a04dd2ef7c7c91cece66
Miguelstarwalker <M.FerreiraGomes.inf25@stud.bbbaden.ch>
2026-09-18 15:49:23 +0200

    Add files via upload

---
+++ Kirby Pong Folder/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            player = new PictureBox();
+            computer = new PictureBox();
+            ball = new PictureBox();
+            GameTimer = new System.Windows.Forms.Timer(components);
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)player).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)computer).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ball).BeginInit();
+            SuspendLayout();
+            // 
+            // player
+            // 
+            player.Image = (Image)resources.GetObject("player.Image");
+            player.Location = new Point(12, 148);
+            player.Name = "player";
+            player.Size = new Size(30, 120);
+            player.SizeMode = PictureBoxSizeMode.StretchImage;
+            player.TabIndex = 0;
+            player.TabStop = false;
+            // 
+            // computer
+            // 
+            computer.Image = Properties.Resources.Untitled27_20260821161431;
+            computer.Location = new Point(758, 148);
+            computer.Name = "computer";
+            computer.Size = new Size(30, 120);
+            computer.SizeMode = PictureBoxSizeMode.StretchImage;
+            computer.TabIndex = 1;
+            computer.TabStop = false;
+            // 
+            // ball
+            // 
+            ball.Image = Properties.Resources.Untitled26_20260821160907;
+            ball.Location = new Point(403, 200);
+            ball.Name = "ball";
+            ball.Size = new Size(30, 30);
+            ball.SizeMode = PictureBoxSizeMode.StretchImage;
+            ball.TabIndex = 2;
+            ball.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            GameTimer.Enabled = true;
+            GameTimer.Interval = 20;
+            GameTimer.Tick += GameTimerEvent;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(82, 8);
+            label1.Name = "label1";
+            label1.Size = new Size(620, 25);
+            label1.TabIndex = 3;
+            label1.Text = "All rights to the characters are reserved to Nintendo and HAL Laboratory, Inc.";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.InactiveBorder;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(798, 452);
+            Controls.Add(ball);
+            Controls.Add(computer);
+            Controls.Add(player);
+            Controls.Add(label1);
+            DoubleBuffered = true;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Player : 0 - Computer : 0";
+            KeyDown += KeyIsDown;
+            KeyUp += KeyIsUp;
+            ((System.ComponentModel.ISupportInitialize)player).EndInit();
+            ((System.ComponentModel.ISupportInitialize)computer).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ball).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox player;
+        private PictureBox computer;
+        private PictureBox ball;
+        private System.Windows.Forms.Timer GameTimer;
+        private Label label1;
+    }
+}
